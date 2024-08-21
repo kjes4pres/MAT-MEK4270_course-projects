d31f47a16d126941eb06d43bfe815d7748fc239f
Kjersti Stangeland <kjesta@eduroam-193-157-238-242.wlan.uio.no>
2024-08-21 15:41:36 +0200

    Added functions, reformatted with black

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -2,10 +2,41 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    pass
+    """
+    Applies a mesh function to a time array.
+
+    Input:
+    f: Python function
+    t: np.ndarray
+
+    Output:
+    mesh: np.ndarray
+    """
+    mesh = np.zeros(len(t))
+    for i in range(len(t)):
+        mesh[i] = f(t[i])
+
+    return mesh
+
 
 def func(t):
-    pass
+    """
+    Applies a mesh function to a time t.
+
+    Input:
+    t: float
+
+    Output:
+    f: float
+    """
+    if 0 <= t <= 3:
+        f = np.exp(-t)
+    elif 3 < t <= 4:
+        f = np.exp(-3 * t)
+    else:
+        raise ValueError("t must be in the range 0, 4.")
+    return f
+
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
