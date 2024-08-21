ce54aec7e5ef8a73061119cfb76037de1ec2b5ca
Kjersti Stangeland <kjesta@eduroam-193-157-238-242.wlan.uio.no>
2024-08-21 16:22:11 +0200

    completed ex. 1.2, tests pass

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,59 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    """
+    Function for differentiating using centered difference scheme.
+
+    Inputs:
+    u: np.ndarray, mesh function
+    dt: float, timestep
+
+    Output:
+    d: np.ndarray, derivative of u
+    """
+
+    n = len(u)  # number of time steps
+    d = np.zeros(n)  # empty array to store derivatives
+
+    # Amplification factor
+    A = 1 / (2 * dt)
+
+    # End points
+    d[0] = (u[1] - u[0]) / dt
+    d[-1] = (u[-1] - u[-2]) / dt
+
+    for i in range(1, n - 1):
+        d[i] = (u[i + 1] - u[i - 1]) * A
+
+    return d
+
 
 def differentiate_vector(u, dt):
-    pass
+    """
+    Function for differentiating using centered difference scheme, vectorized.
+
+    Inputs:
+    u: np.ndarray, mesh function
+    dt: float, timestep
+
+    Output:
+    d: np.ndarray, derivative of u
+    """
+
+    n = len(u)  # number of time steps
+    d = np.zeros(n)  # empty array to store derivatives
+
+    # Amplification factor
+    A = 1 / (2 * dt)
+
+    # End points
+    d[0] = (u[1] - u[0]) / dt
+    d[-1] = (u[-1] - u[-2]) / dt
+
+    d[1:-1] = (u[2:] - u[0:-2]) * A
+
+    return d
+
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
@@ -15,6 +64,6 @@ def test_differentiate():
     du2 = differentiate_vector(u, dt)
     assert np.allclose(du1, du2)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test_differentiate()
-    
